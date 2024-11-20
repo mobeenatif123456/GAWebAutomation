89b6f403fab9bae4325de4bf9c492b0fb28df1cf
mobeenatif123456 <73457508+mobeenatif123456@users.noreply.github.com>
2024-11-21 00:23:14 +0500

    brad report test cases

--- src/main/java/webAutomationGA/pageObjects/bradReport.java
+++ src/main/java/webAutomationGA/pageObjects/bradReport.java
@@ -1,10 +1,18 @@
 package webAutomationGA.pageObjects;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import webAutomationGA.abstractComponents.abstractComponent;
 
@@ -22,39 +30,194 @@ public class bradReport extends abstractComponent {
 	}
 	
 	
-	@FindBy(xpath="//h3[text()='Customer ID']")
-	WebElement customerID;
+	@FindBy(xpath="//h3[text()='Year / Period']")
+	WebElement yearChannel;
+	
+	@FindBy(xpath="//h3[text()='Year / Period']/../../../..//div[text()='All'] | //h3[text()='Year / Period']/../../../..//div[text()='2024'] |  //h3[text()='Year / Period']/../../../..//div[text()='Multiple selections']")
+	WebElement allDropdown;
+	
+	@FindBy(xpath="//span[@class='slicerText']")
+    List<WebElement> dropdownOptions;
+	
+	@FindBy(xpath="//div[@role='columnheader']/..")
+	WebElement columnHeaders;
+	
+	
+	@FindBy(xpath="//span[text()='2019']")
+	WebElement selectedYearChannel;
+	
+	@FindBy(xpath="//div[text()='Year']")
+	WebElement yearHeader;
+	
+	@FindBy(xpath="(//div[text()='Year']/../../../../..//div[@role='gridcell'])[2]")
+	WebElement firstRowYear;
+	
+	@FindBy(xpath="(//div[text()='Order Type Desc']/../../../../..//div[@role='gridcell'])[5]")
+	WebElement firstRowOrderTypeDesc;
+	
+	@FindBy(xpath="//button[text()='Reset']")
+	WebElement resetButton;
+	
+	@FindBy(xpath="//span[text()='Brad Report']")
+	WebElement bradReportHeading;
+	
+	@FindBy(xpath="//div[@class='imageBackground']")
+    WebElement filterButton;
+	
+	@FindBy(xpath="//*[name()='svg']/..//h3[text()='Order Type']")
+    WebElement orderType;
+	
+	@FindBy(xpath="//*[name()='svg']/..//h3[text()='Order Type']/../../../..//div[text()='All']")
+    WebElement orderTypeDropdown;
+	
+	
+	@FindBy(xpath="(//div[@class='searchHeader show']/input)[1]")
+    WebElement orderTypeSearch;
+	
+	@FindBy(xpath="//span[text()='STANDARD ORDER']")
+    WebElement standardOrder;
+	
+	@FindBy(xpath="//div[@class='imageBackground']")
+    WebElement crossButton;
 	
 	@FindBy(xpath="//h3[text()='Available Columns']/../../../..//div[text()='All']")
-	WebElement allColumnsDropdown;
+    WebElement availableColumnsDropdown;
+	
+	@FindBy(xpath="//span[text()='Year']")
+    WebElement availableColumnsYear;
+	
+	@FindBy(xpath="//span[text()='Period']")
+    WebElement availableColumnsPeriod;
+	
+	@FindBy(xpath="//span[text()='Date']")
+    WebElement availableColumnsDate;
+	
+	@FindBy(xpath="//span[text()='Order Type Desc']")
+    WebElement orderTypeDesc;
 	
 	
+	@FindBy(xpath="//div[text()='Order Type Desc']")
+    WebElement orderTypeDescColumnHeader;
 	
 
-	public void dropdownVisible() throws InterruptedException {
+	
+	public void channelFilter() throws InterruptedException {
 		
-		waitforElementDisplayed(By.xpath("//h3[text()='Customer ID']"));
-		waitforElementDisplayed(By.xpath("//h3[text()='Interaction ID']"));
-		waitforElementDisplayed(By.xpath("//h3[text()='Year / Period']"));
-		waitforElementDisplayed(By.xpath("//h3[text()='Available Columns']"));
+		
+		commonActions();
+		waitforElementDisplayed(By.xpath("//h3[text()='Year / Period']/../../../..//div[text()='All'] | //h3[text()='Year / Period']/../../../..//div[text()='2024'] |  //h3[text()='Year / Period']/../../../..//div[text()='Multiple selections']"));
+		allDropdown.click();
+		Thread.sleep(5000);
+		
 		
 	}
 	
-public void valuesUnderAvailableColumns() throws InterruptedException {
 	
 	
-	waitforElementDisplayed(By.xpath("//h3[text()='Available Columns']/../../../..//div[text()='All']"));
-	allColumnsDropdown.click();
-	waitforElementDisplayed(By.xpath("//div[@title='Year']"));
-	waitforElementDisplayed(By.xpath("//div[@title='Period']"));
-	waitforElementDisplayed(By.xpath("//div[@title='Date']"));
-	waitforElementDisplayed(By.xpath("//div[@title='Delivery Date']"));
-	waitforElementDisplayed(By.xpath("//div[@title='Header Doc Date']"));
-	waitforElementDisplayed(By.xpath("//div[@title='Request Delivery Date']"));
+	
+	public List<String> verifyYearOptions() {
+	    
+	    String[] expectedYears = {"Select all","2019", "2020", "2021", "2022", "2023", "2024"};
+	    List<String> missingYears = new ArrayList<>();
 
+	    for (String year : expectedYears) {
+	        boolean yearFound = dropdownOptions.stream()
+	                .anyMatch(option -> option.getText().trim().equals(year));
+	        if (!yearFound) {
+	            missingYears.add(year); // Add missing years to the list
+	        }
+	    }
+	    return missingYears;
+	}
+	
+	
+	
+	public String selectingYearandValidatingResults(String selectedYear) throws InterruptedException {
+		
+		bradReportHeading.click();
+		Thread.sleep(1000);
+		waitforElementDisplayed(By.xpath("//div[text()='Year']"));
+		Thread.sleep(1000);
+		yearHeader.click();
+		Thread.sleep(15000);
+		String year = firstRowYear.getText();
+		return year;
 		
 	}
 	
 	
+	public void waitForSpinnerToDisappear() {
+		WebElement revealed = driver.findElement(By.xpath("//div[@class='powerbi-spinner xsmall']"));
+		Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+	    wait.until(d -> revealed.isDisplayed());
+	}
+
+	public void filtersFunction() throws InterruptedException {
+		
+		
+		commonActions();
+		filterButton.click();
+		Thread.sleep(5000);
+		waitforElementDisplayed(By.xpath("//*[name()='svg']/..//h3[text()='Order Type']"));
+		Thread.sleep(1000);
+		orderTypeDropdown.click();
+		Thread.sleep(5000);
+		orderTypeSearch.sendKeys("STANDARD ORDER");
+		standardOrder.click();
+		Thread.sleep(5000);
+		crossButton.click();
+		Thread.sleep(2000);
+		availableColumnsDropdown.click();
+		Thread.sleep(1000);
+		availableColumnsYear.click();
+		availableColumnsPeriod.click();
+		availableColumnsDate.click();
+		orderTypeSearch.sendKeys("Order Type Desc");
+		Thread.sleep(1000);
+		orderTypeDesc.click();
+		Thread.sleep(5000);
+		bradReportHeading.click();
+		
+	}
+	
+public String filtersVerification() throws InterruptedException {
+		
+		
+		waitforElementDisplayed(By.xpath("//div[text()='Order Type Desc']"));
+		Thread.sleep(1000);
+		orderTypeDescColumnHeader.click();
+		Thread.sleep(15000);
+		String ordertypeDesc = firstRowOrderTypeDesc.getText();
+		
+		return ordertypeDesc;
+	
+		
+	}
+	
+	
+	public void commonActions() throws InterruptedException {
+		
+		waitforElementDisplayed(By.xpath("//div[@role='columnheader']/.."));
+		Thread.sleep(1000);
+		
+		try {
+	        WebElement element = driver.findElement(By.xpath("//button[@class='resetBtn rightActionBarBtn ng-star-inserted']"));
+
+	        // Check if the element is visible (displayed)
+	        if (element.isDisplayed()) {
+	            element.click();
+	            System.out.println("Element clicked successfully.");
+	            resetButton.click();
+	            Thread.sleep(1000);
+	            waitforElementDisplayed(By.xpath("//div[@role='columnheader']/.."));
+	            
+	        } else {
+	            System.out.println("Element is not visible, skipping click.");
+	        }
+	    } catch (NoSuchElementException e) {
+	        System.out.println("Element not found, skipping click.");
+	    }
+		
+	}
 
 }
--- src/test/java/webAutomationGA/Tests/bradReportPage.java
+++ src/test/java/webAutomationGA/Tests/bradReportPage.java
@@ -1,7 +1,9 @@
 package webAutomationGA.Tests;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,20 +18,40 @@ public class bradReportPage extends baseTest {
     public void setup() throws IOException, InterruptedException {
         // Perform login only once
         landingpage.login("mobeen.atif@effem.com", "Kabul@12345678");
-
-        // Initialize bradReport object after login
         test = new bradReport(driver);
     }
 
-    @Test(priority = 0, groups = {"smokeTests"}, description = "Verify brad report dropdowns are present")
-    public void dropdownVisible() throws InterruptedException {
-        // Verify dropdown visibility
-        test.dropdownVisible();
+    @Test(priority = 0, groups = {"smokeTests"}, description = "Verify brad report year/channel filter is working")
+    public void channelFilter() throws InterruptedException {
+        
+        test.channelFilter();
+        List<String> missingYears = test.verifyYearOptions();
+        Assert.assertTrue(missingYears.isEmpty(), "Missing years in the dropdown: " + missingYears);
+        String selectedYear = "2024";
+        String ascYear= test.selectingYearandValidatingResults(selectedYear);
+        Assert.assertEquals(selectedYear, ascYear, "The strings are not equal!");
+        String desYear= test.selectingYearandValidatingResults(selectedYear);
+        Assert.assertEquals(selectedYear, desYear, "The strings are not equal!");
+        
+        
     }
-
-    @Test(priority = 1, groups = {"smokeTests"}, description = "Verify values under available columns are present")
-    public void availableColumnValues() throws InterruptedException {
-        // Verify values under available columns
-        test.valuesUnderAvailableColumns();
+    
+    @Test(priority = 0, groups = {"smokeTests"}, description = "Verify brad report filters are working")
+    public void filters() throws InterruptedException {
+        
+    	
+    	test.filtersFunction();
+    	
+        String text= "STANDARD ORDER";
+        String orderTypeAsc= test.filtersVerification();
+        Assert.assertEquals(text, orderTypeAsc, "The strings are not equal!");
+        String orderTypeDesc= test.filtersVerification();
+        Assert.assertEquals(text, orderTypeDesc, "The strings are not equal!");
+        
+        
     }
+    
+    
+    
+    
 }
